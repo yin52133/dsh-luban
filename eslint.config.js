29931714b7cc1ae06c99828161134337735d13d0
yin52133 <yin52133@users.noreply.github.com>
2026-09-02 00:00:54 +0800

    chore(lint): ignore release artifacts

--- eslint.config.js
+++ eslint.config.js
@@ -12,6 +12,7 @@ export default tseslint.config(
       '**/.acceptance/**',
       '**/.runtime/**',
       '**/.luban/**',
+      '**/.release-artifacts/**',
     ],
   },
   eslint.configs.recommended,
